909afad6f17663f6ea59ecb60099946048a8c919
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-28 17:34:33 +0000

    Done.

--- app/shows_controller.rb
+++ app/shows_controller.rb
@@ -1,7 +1,7 @@
 
 def highest_rating
   Show.maximum(:rating)
-  
+
 end
 
 def most_popular_show
@@ -24,7 +24,10 @@ def ratings_sum
 end
 
 def popular_shows
+  #returns an array of all shows with a rating above 5
 end
 
 def shows_by_alphabetical_order
+  #returns an array of all shows, listed in alphabetical order
+  
 end

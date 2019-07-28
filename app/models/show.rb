2e9e274cbc0b3d65908b7ca92140ca39e3db10bd
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-28 17:29:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,33 @@ class Show < ActiveRecord::Base
   def Show::highest_rating
     Show.maximum(:rating)
   end
+
+  def Show::most_popular_show
+    show = Show.maximum(:rating)
+    show
+  end
+
+  def Show::lowest_rating
+    show = Show.minimum(:rating)
+    show
+  end
+
+  def Show::least_popular_show
+    show = Show.minimum(:rating)
+    show
+  end
+
+  def Show::ratings_sum
+    Show.sum(:rating)
+  end
+
+  def Show::popular_shows
+    #returns an array of all shows with a rating above 5
+  end
+
+  def Show::shows_by_alphabetical_order
+    #returns an array of all shows, listed in alphabetical order
+
+  end
+
 end
